54dd5556c2c1662a16ece2c9a5266bfc0593063e
Andrei Shinkarev <ashinkarev@datamola.com>
2023-03-19 12:00:39 +0300

    fix: add validate interface

---
+++ src/auth/strategies/jwt.interface.ts
@@ -0,0 +1,7 @@
+export interface JwtValidate {
+  login: string;
+  userName: string;
+  userId: number;
+  iat: number;
+  exp: number;
+}
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,18 +4,19 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from 'src/users/entity/users.entity';
 import { UsersService } from 'src/users/users.service';
 import { STRATEGY_NAME } from '../auth.constants';
+import { JwtValidate } from './jwt.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, STRATEGY_NAME.jwt) {
   constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: process.env.AUTH_SECRET,
     });
   }
 
-  public async validate(user: User): Promise<User> {
-    return await this.usersService.findUserById(user.id);
+  public async validate(user: JwtValidate): Promise<User> {
+    return await this.usersService.findUserById(user.userId);
   }
 }
